a0891ebfb49e609ccb73cd2301ff812414b0d018
alistairEjenkins <37299657+alistairEjenkins@users.noreply.github.com>
2020-12-18 18:42:45 +0000

    Add files via upload

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -17,20 +17,15 @@ while len(found_states) < 50:
     another_state = screen.textinput(title=f'{len(found_states)} / 50 states found', prompt='Name another state')
     another_state = another_state.title()
     if another_state == 'Exit':
-        missing_states = []
-        for state in data.state:
-            if state not in found_states:
-                missing_states.append(state)
+        missing_states = [state for state in data.state if state not in found_states]
+        df = pandas.DataFrame(missing_states)
+        df.to_csv('states_to_learn.csv')
         break
     for n in range(50):
         if data.iloc[n].state == another_state and another_state not in found_states:
             t.goto(data.iloc[n].x, data.iloc[n].y)
             t.write(another_state)
             found_states.append(another_state)
-
-state_dict = {'state': missing_states}
-df = pandas.DataFrame(state_dict)
-df.to_csv('states_to_learn.csv')
 
 screen.exitonclick()
 
